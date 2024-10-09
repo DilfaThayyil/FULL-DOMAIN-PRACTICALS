83c9eb912dc38781df6bcbc97fd1a066a9b78182
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-09 18:26:36 +0530

    stringMethods

---
+++ JAVASCRIPT/StringMethods.js
@@ -0,0 +1,20 @@
+//toupperCase
+let string= 'shibil'
+let a = string.toUpperCase()
+console.log(a)
+
+//toLowerCase
+let string1 = 'DilFA ThayYiL'
+let b = string1.toLowerCase()
+console.log(b);
+
+//slice
+let string2 = 'Heyy, Cutiee..'
+let salute = string2.slice(0,5)
+console.log(salute)
+
+//split
+let string3 = 'Apple,banana,orange,grape'
+let splitted = string3.split(',',2)
+console.log(splitted)
+
