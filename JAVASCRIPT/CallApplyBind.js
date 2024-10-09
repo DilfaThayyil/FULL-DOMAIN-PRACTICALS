58ce04cd9c6d19044f6f20ddc8e981c097b9d6a1
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-09 21:13:30 +0530

    call apply bind added

---
+++ JAVASCRIPT/CallApplyBind.js
@@ -0,0 +1,29 @@
+//Call
+function introduce(greeting) {
+    console.log(greeting + ', my name is ' + this.name);
+  }
+  
+  const person = { name: 'Dilfa' };
+  
+  introduce.call(person, 'Hello');
+  
+
+//Apply
+  function introduce(greeting, punctuation) {
+    console.log(greeting + ', my name is ' + this.name + punctuation);
+  }
+  
+  const person1 = { name: 'Dilfa' };
+  
+  introduce.apply(person, ['Hi', '!']);  
+  
+//Bind
+  function introduce(greeting) {
+    console.log(greeting + ', my name is ' + this.name);
+  }
+  
+  const person2 = { name: 'Dilfa' };
+  
+  const boundIntroduce = introduce.bind(person); 
+  boundIntroduce('Hey');  
+  
--- JAVASCRIPT/TypeConversions.js
+++ JAVASCRIPT/TypeConversions.js
@@ -9,3 +9,16 @@ console.log(str1)
 let str2 = '123'
 let num2 = Number(str2)
 console.log(num2)
+
+let a = 3
+a = 'dilfa'
+console.log(a) //correct
+
+var aaa = 5
+aaa = 'thayyil' //also correct
+console.log(aaa)
+
+const aa = 4
+aa = 'dil'  //TypeError: Assignment to constant variable.
+console.log(aa)
+
