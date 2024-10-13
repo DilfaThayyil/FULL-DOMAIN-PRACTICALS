e1c0d9e097145183d1342fea4dc7895860b5ec8e
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-13 19:04:51 +0530

    findSecondLargestEvenNumber , findSum using reduce

--- JAVASCRIPT/reduce.js
+++ JAVASCRIPT/reduce.js
@@ -1,3 +1,5 @@
+
+//find SecondLargest EvenNumber
 function findSecLargest(arr){
     let result = arr.reduce((acc,curr)=>{
         if(curr%2===0){
@@ -6,5 +8,19 @@ function findSecLargest(arr){
                 acc.largest = curr
             }
         }
-    })
+        return acc
+    },{largest:-Infinity,secLargest:-Infinity})
+    return result.secLargest===-Infinity?null:result.secLargest
 }
+console.log(findSecLargest([4,7,5,3,5,8]))
+
+
+
+//findSum
+function sum(arr){
+    let sum = arr.reduce((acc,curr)=>{
+        return acc+curr
+    },0)
+    return sum
+}
+console.log(sum([1,2,3,4,5]))
