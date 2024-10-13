a037cf7332fa504d226115a94c4eb46888af376d
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-13 20:12:53 +0530

    2D array

---
+++ JAVASCRIPT/2Darray.js
@@ -0,0 +1,30 @@
+
+
+// //flatten 2D array
+// function flatten(arr){
+//     let result = []
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=0;j<arr[i].length;j++){
+//             result.push(arr[i][j])
+//         }
+//     }
+//     return result
+// }
+// const arr = [[1,2],[3,4],[5,6]]
+// console.log(flatten(arr))
+
+
+//yield numbers from a 2D array using Generator fn
+function* yieldNumbers(arr){
+    for(let row of arr){
+        for(let num of row){
+            yield num
+        }
+    }
+}
+
+const arr = [[1,2],[3,4],[5,6]]
+const generator = yieldNumbers(arr)
+for(let num of generator){
+    console.log(num)
+}
