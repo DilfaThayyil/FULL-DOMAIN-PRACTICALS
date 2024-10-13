f3077be5c518e0f5743e3535c252ef1e7d6c49e2
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-13 20:17:02 +0530

    2D changes

--- JAVASCRIPT/2Darray.js
+++ JAVASCRIPT/2Darray.js
@@ -1,17 +1,19 @@
 
 
-// //flatten 2D array
-// function flatten(arr){
-//     let result = []
-//     for(let i=0;i<arr.length;i++){
-//         for(let j=0;j<arr[i].length;j++){
-//             result.push(arr[i][j])
-//         }
-//     }
-//     return result
-// }
-// const arr = [[1,2],[3,4],[5,6]]
-// console.log(flatten(arr))
+//flatten 2D array
+function flatten(arr){
+    let result = []
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            result.push(arr[i][j])
+        }
+    }
+    return result
+}
+const arr = [[1,2],[3,4],[5,6]]
+console.log(flatten(arr))
+
+
 
 
 //yield numbers from a 2D array using Generator fn
@@ -22,9 +24,15 @@ function* yieldNumbers(arr){
         }
     }
 }
-
-const arr = [[1,2],[3,4],[5,6]]
-const generator = yieldNumbers(arr)
+const arr1 = [[1,2],[3,4],[5,6]]
+const generator = yieldNumbers(arr1)
 for(let num of generator){
     console.log(num)
 }
+
+
+
+//using flat method
+let arr2 = [[1,2],[3,4],[5,6]]
+flattedArray = arr2.flat()
+console.log(flattedArray)
