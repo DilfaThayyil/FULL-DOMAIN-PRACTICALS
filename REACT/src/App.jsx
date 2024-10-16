3d7d6a3ee80f70bc53c94a1bdd5a3821d8172c88
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-16 19:31:56 +0530

    fetched dummydata and displayed it

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,14 +1,60 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [posts,setPosts] = useState(null)
+  const [posts,setPosts] = useState([])
+  const [showData,setShowData] = useState(false)
+
+  const fetchPosts = async()=>{
+    try{
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      setPosts(response.data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchPosts()
+  },[])
+
+  const handleData = ()=>{
+    setShowData(true)
+  }
 
   return (
-    <>
-    </>
+      <div>
+        {!showData &&(
+          <button onClick={handleData}>Click to fetch Data</button>
+        )}
+        {showData &&(
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>UserId</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post)=>{
+                return (
+                  <tr key={post.id}>
+                    <td>{post.id}</td>
+                    <td>{post.userId}</td>
+                    <td>{post.title}</td>
+                    <td>{post.body}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
   )
 }
 
