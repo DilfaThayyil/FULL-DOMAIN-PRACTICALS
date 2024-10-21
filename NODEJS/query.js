87d6ae0289dcf84cb49836c303239ebbcbc1fd0c
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-21 17:12:34 +0530

    query created

---
+++ NODEJS/query.js
@@ -0,0 +1,15 @@
+//query parameters
+app.get('/',(req,res)=>{
+    console.log(req.query)
+})
+
+//URL parameters
+app.get('/',(req,res)=>{
+    console.log(req.params.id)
+})
+
+//URL consist of 
+// 1. HTTP (protocol)
+// 2. Domain(examle.com)
+// 3. path(/user)
+// 4. query parameters(?=search)
