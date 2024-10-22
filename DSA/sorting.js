5895e8b645e95e3ee44e21e4a6fe825d7cc0e806
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-22 21:20:39 +0530

    quick sort and insertion sort

--- DSA/sorting.js
+++ DSA/sorting.js
@@ -1,5 +1,5 @@
 function bubbleSort(arr){
-    let swapped 
+    let swapped
     do{
         swapped = false
         for(let i=0;i<arr.length-1;i++){
@@ -7,14 +7,50 @@ function bubbleSort(arr){
                 let temp = arr[i]
                 arr[i] = arr[i+1]
                 arr[i+1] = temp
-                swapped = true
             }
         }
     }while(swapped)
 }
 
-const arr = [3,5,7,2,3]
+const arr = [8,2,3,0,4,6]
 bubbleSort(arr)
 console.log(arr)
+
+
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let numToInsert = arr[i]
+        let j = i-1
+        while(j>=0&&arr[j]>numToInsert){
+            arr[j+1] = arr[j]
+            j = j-1
+        }
+        arr[j+1] = numToInsert
+    }
+}
+
+const arr1 = [7,2,4,5,9,0]
+insertionSort(arr1)
+console.log(arr1)
+
+function quickSort(arr){
+    if(arr.length<2){
+        return arr
+    }
+    let pivot = arr[arr.length-1]
+    let left = []
+    let right = []
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left),pivot,...quickSort(right)]
+}
+
+const arr2 = [6,3,5,8,4,2]
+console.log(quickSort(arr2))
 
 
