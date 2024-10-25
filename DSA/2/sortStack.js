88cc1511dced2c75b32bced8e03ea300d9bd96bc
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-25 21:04:09 +0530

    .....

---
+++ DSA/2/sortStack.js
@@ -0,0 +1,19 @@
+//sort stack using temporary stack
+
+function sortStack(originalStack){
+    let tempStack = []
+    while(originalStack.length>0){
+        let temp = originalStack.pop()
+        while(tempStack.length>0 && tempStack[tempStack.length-1]<temp){
+            originalStack.push(tempStack.pop())
+        }
+        tempStack.push(temp)
+    }
+    while(tempStack.length>0){
+        originalStack.push(tempStack.pop())
+    }
+    return originalStack
+}
+
+let stack = [5,2,4,9,6,0]
+console.log(sortStack(stack))
