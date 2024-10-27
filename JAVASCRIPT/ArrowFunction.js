e4153cec08797b770afee3c9c51c5d13778abbf9
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-27 17:04:20 +0530

    arrow fucntions

--- JAVASCRIPT/ArrowFunction.js
+++ JAVASCRIPT/ArrowFunction.js
@@ -1,2 +1,21 @@
 let add = (a,b)=>a+b
 //shorter syntax for writing functions
+
+let person = {
+    name : "Dilfa",
+    age : 20,
+    greeting : ()=>{
+        console.log(`Hello ${this.name}`) //Hello undefined
+    }
+}
+person.greeting()
+
+
+let person1 = {
+    name : "Shibil",
+    city : "pattambi",
+    greeting(){
+        console.log(`Hello ${this.name}`) //Hello shibil
+    }
+}
+person1.greeting()
