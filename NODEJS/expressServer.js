5a3a3b10352d40733d80880ff7c172a6eebf6c00
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-27 17:06:12 +0530

    expressServer

--- NODEJS/expressServer.js
+++ NODEJS/expressServer.js
@@ -1,7 +1,18 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello world')
+// })
+// app.listen(3000)
+
+
 const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
-app.listen(3000)
+app.listen(5000,()=>{
+    console.log('server listening on port 5000')
+})
