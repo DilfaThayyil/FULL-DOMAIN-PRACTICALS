809d9d7737ff634461fb759ca53a066031ec9047
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-28 20:13:49 +0530

    arrow function this context

--- JAVASCRIPT/ArrowFunction.js
+++ JAVASCRIPT/ArrowFunction.js
@@ -1,21 +1,31 @@
-let add = (a,b)=>a+b
-//shorter syntax for writing functions
+// let add = (a,b)=>a+b
+// //shorter syntax for writing functions
 
-let person = {
-    name : "Dilfa",
-    age : 20,
-    greeting : ()=>{
-        console.log(`Hello ${this.name}`) //Hello undefined
-    }
-}
-person.greeting()
+// let person = {
+//     name : "Dilfa",
+//     age : 20,
+//     greeting : ()=>{
+//         console.log(`Hello ${this.name}`) //Hello undefined
+//     }
+// }
+// person.greeting()
 
 
-let person1 = {
-    name : "Shibil",
-    city : "pattambi",
-    greeting(){
-        console.log(`Hello ${this.name}`) //Hello shibil
-    }
-}
-person1.greeting()
+// let person1 = {
+//     name : "aaaa",
+//     city : "bbbb",
+//     greeting(){
+//         console.log(`Hello ${this.name}`) //Hello aaaa
+//     }
+// }
+// person1.greeting()
+
+
+
+//using arrow function as a constructor
+const Person = (name) => {
+    this.name = name;
+  };
+  // This will throw an error
+  const john = new Person('John'); //Person is not a constructor bcuz it uses an arrow fn (arrow fn doesn't have its own this context)
+  
