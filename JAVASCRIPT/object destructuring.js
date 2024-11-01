a14b38c721ec3ad26a23884e47fd38e3cae523dd
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-01 09:38:29 +0530

    array desctructuring

--- JAVASCRIPT/object destructuring.js
+++ JAVASCRIPT/object destructuring.js
@@ -5,18 +5,14 @@
 // console.log(age)
 
 
-/// call apply bind
-
-
-function greet(name){
-    console.log(`${name}`)
-}
-const person = {
-    name : 'Alice'
-}
-
-greet.call(person)
-greet.apply(person,['Hi'])
-
-const greetAlice = greet.bind(person,'Hey')
-greetAlice()
+//array destructuring
+// let arr = [1,2]
+// const [first,second,third] = arr //normal destructuring
+// const {first:one,second,third} = arr //default value
+// const [first,,third] = arr //leaving blank space
+// const [first,...rest] = arr
+let a = 1;
+let b = 2;
+[a,b] = [b,a]
+console.log(a)
+console.log(b)
