fe43155d7ad22f844ad3642cc8501793ac5cb8c4
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-03 12:31:46 +0530

    promise

--- JAVASCRIPT/Promise.js
+++ JAVASCRIPT/Promise.js
@@ -12,13 +12,12 @@
 // promise.then((result)=>console.log(result))
 
 
-const p1 = Promise.resolve(1)
-const p2 = Promise.resolve(2)
-const p3 = Promise.reject('error')
-Promise.all([p1,p2,p3])
-    .then(value => console.log(value))
-    .catch(error => console.log('caught an error',error))
-
+// const p1 = Promise.resolve(1)
+// const p2 = Promise.resolve(2)
+// const p3 = Promise.reject('error')
+// Promise.all([p1,p2,p3])
+//     .then(value => console.log(value))
+//     .catch(error => console.log('caught an error',error))
 
 
 // const p1 = new Promise(resolve => setTimeout(()=>resolve('First'),1000))
@@ -35,3 +34,6 @@ Promise.all([p1,p2,p3])
 // const p2 = Promise.resolve('Success')
 // Promise.any([p1,p2]).then(value => console.log(value))
 
+
+
+//practice
