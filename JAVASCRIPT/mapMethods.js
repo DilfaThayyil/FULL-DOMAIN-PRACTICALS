6467cc7ec6728e57533d14586c20f35f1e06e5cc
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-03 13:21:05 +0530

    map

---
+++ JAVASCRIPT/mapMethods.js
@@ -0,0 +1,11 @@
+// let map = new Map([["name","dil"],["age",20],[true,"yes"]]) //new Map
+// console.log(map)
+// console.log(map.size)                                       //size
+// map.set("key","value")                                      //set
+// console.log(map)
+// console.log(map.has("keyy"))                                //has
+// console.log(map.get("name"))                                //get
+// map.delete("key")                                           //delete
+// console.log(map.has("key"))
+// map.clear()                                                 //clear
+// console.log(map.size)                                       //size
