be8455785097bb02d73a129b4bdb68f11e27908a
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-03 13:21:25 +0530

    object

--- JAVASCRIPT/objectProblems.js
+++ JAVASCRIPT/objectProblems.js
@@ -1,18 +1,16 @@
 //remove last property of an obj
-const obj = {a:1,b:2,c:3}
 // using built-in methods
+const obj = {a:1,b:2,c:3}
 const keys = Object.keys(obj)
 if(keys.length>0){
     let lastKey = keys[keys.length-1]
     delete obj[lastKey]
 }
 console.log(obj)
-
-// -----------------------------------
-
+                                                    //using "delete" method in obj
 //remove last property of an obj
-const obj1 = {a:1,b:2,c:2}
 //without using built-in methods
+const obj1 = {a:1,b:2,c:2}
 let lastKey
 for(let key in obj1){
     lastKey = key
