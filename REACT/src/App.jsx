f01705747bead948e8b6bfa56b22e47cdd8c0ee9
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-08 10:24:30 +0530

    setup

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react'
+import { useState , useEffect, useReducer } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,11 +13,16 @@ import Counter from './pages/customHookParent'
 
 
 function App() {
+   const [count,setCount] = useState(0)
+
  
    return (
-    <>
-    <Counter/>
-    </>
+    <div>
+      <h1>count : {count}</h1>
+      <button onClick={()=>setCount((prev)=>prev+1)}>increment</button>
+      <button onClick={()=>setCount((prev)=>prev-1)}>decrement</button>
+      <button onClick={()=>setCount(0)}>reset</button>
+    </div>
    )
 
 }
