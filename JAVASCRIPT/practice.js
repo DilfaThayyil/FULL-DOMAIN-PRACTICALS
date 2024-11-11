95cdecb46dc3843d38c02eb19766fc9ef3e3df53
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-11 20:31:08 +0530

    dsgd

--- JAVASCRIPT/practice.js
+++ JAVASCRIPT/practice.js
@@ -392,17 +392,73 @@
 // }
 // ---------------------------------
 //fibonacci series using recursion
-
+function fibonacci(n){
+    if(n===0){
+        return 0
+    }else if(n===1){
+        return [0,1]
+    }
+    let series = fibonacci(n-1)
+    series.push(series[n-1]+series[n-2])
+    return series
+}
 // ---------------------------------
 //fibonacci series
-
+    function fibonacc(n){
+        let fib = [0,1]
+        for(let i=2;i<=n;i++){
+            fib[i] = fib[i-1]+fib[i-2]
+        }
+        return fib
+    }
 // -----------------------------------
 //fibonacci of nth number
-
+    function fib(n){
+        if(n<2){
+            return n
+        }
+        return fib(n-1)+fib(n-2)
+    }
+    console.log(fibonacci(5))
+    console.log(fibonacc(5))
+    console.log(fib(5))
 // ----------------------------------
 //factorial using recursion
-
+    function factorial(n){
+        if(n<=1){
+            return n
+        }
+        return n*factorial(n-1)
+    }
 // -----------------------------------
 //factorial of nth number
-
+    function fact(n){
+        let result = 1
+        for(let i=2;i<=n;i++){
+            result = result*i
+        }
+        return result
+    }
+    console.log(factorial(5))
+    console.log(fact(5))
 // ---------------------------------
+//recursive binary search
+function binarySearch(arr,target,left=0,right=arr.length){
+    while(left<right){
+        let mid = Math.floor((left+right)/2)
+        if(arr[mid]===target){
+            return mid
+        }else if(target>arr[mid]){
+            return binarySearch(arr,target,mid+1,right)
+        }else{
+            return binarySearch(arr,target,left,mid-1)
+        }
+    }
+    return -1
+}
+//binary search
+function binarySearch(arr,target,left=0,right=arr.length){
+    
+}
+let index = binarySearch([2,3,4,5,6],6)
+console.log(index)
