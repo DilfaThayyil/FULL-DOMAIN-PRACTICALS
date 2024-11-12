fa4d764c809f342842bfe44b2879173f7591201b
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-12 09:00:19 +0530

    practice

--- DSA/practice.js
+++ DSA/practice.js
@@ -1306,8 +1306,112 @@ class LinkedList{
             if(this.head===this.tail){
                 this.head = null
                 this.tail = null
+            }else{
+                this.head = this.head.next
+                this.head.prev = null
+            }
+            return
+        }
+        let curr = this.head
+        while(curr&&curr.value!==value){
+            curr = curr.next
+        }
+        if(!curr){
+            console.log('value not found')
+        }
+        if(curr===this.tail){
+            this.tail = curr.prev
+            this.tail.next = null
+        }else{
+            curr.prev.next = curr.next
+            if(curr.next){
+                curr.next.prev = curr.prev
             }
         }
+        return
     }
 }
 
+
+
+function bubbleSort(arr){
+    let swapped
+    do{
+        swapped = false
+        for(let i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }
+        }
+    }while(swapped)
+
+    return arr
+}
+
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let numToIns = i
+        let j = i-1
+        while(j>=0&&arr[j]>numToIns){
+            arr[j+1] = arr[j]
+            j = j+1
+        }
+        arr[j+1] = numToIns
+    }
+    return arr
+}
+
+function selectionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let minIndex = i
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]>arr[minIndex]){
+                minIndex = j
+            }
+        }
+        if(minIndex!==i){
+            let temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+        }
+    }
+    return arr
+}
+
+function quickSort(arr){
+    let pivot = arr[arr.length-1]
+    let left = []
+    let right = []
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return arr
+}
+
+function mergeSort(arr){
+    if(arr.length<2){
+        return arr
+    }
+    let mid = Math.floor((arr.length/2))
+    let leftArr = arr.slice(0,mid)
+    let rightArr = arr.slice(mid)
+    return merge(mergeSort(leftArr),mergeSort(rightArr))
+}
+function merge(left,right){
+    let sorted = []
+    while(left.length&&right.length){
+        if(left[0]<=right[0]){
+            sorted.push(left.shift())
+        }else{
+            sorted.push(right.shift())
+        }
+    }
+    return [...sorted,...left,...right]
+}
