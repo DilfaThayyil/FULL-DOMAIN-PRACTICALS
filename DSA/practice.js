0e9b17972e90178a70dd685d46604a5871ef0fdf
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-12 13:37:00 +0530

    binarysearch

--- DSA/practice.js
+++ DSA/practice.js
@@ -1918,6 +1918,48 @@ class LinkedList{
             this.head = temp.prev
         }
     }
+    
 }
 
 
+//linear search
+function linearSearch(arr,target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            return i
+        }
+    }
+    return -1
+}
+
+//binary search
+function binarySearch(arr,target){
+    let left = 0
+    let right = arr.length-1
+    let mid = Math.floor((left+right)/2)
+    while(left<right){
+        if(arr[mid]===target){
+            return mid
+        }else if(arr[mid]>target){
+            left = mid-1
+        }else{
+            right = mid+1
+        }
+    }
+    return -1
+}
+
+//recursive binarySearch
+function binarySearch(arr,target,left=0,right=arr.length-1){
+    if(left>right){
+        return -1
+    }
+    let mid = Math.floor((left+right)/2)
+    if(arr[mid]===target){
+        return mid
+    }else if(arr[mid]>target){
+        return binarySearch(arr,target,mid-1,right)
+    }else{
+        return binarySearch(arr,target,left,mid+1)
+    }
+}
