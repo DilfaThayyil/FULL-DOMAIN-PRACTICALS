8f877983d59297d6c84dbb86e060e6fc12f9d0ba
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-16 15:42:55 +0530

    callback hell

---
+++ JAVASCRIPT/callbackHell.js
@@ -0,0 +1,71 @@
+
+
+// // //callback hell
+// // --------------------------------------------------------------------------------------
+// // function fetchData(callback){
+// //     setTimeout(()=>{
+// //         callback(null,`data fetched`)
+// //     },1000)
+// // }
+// // function processData(data,callback){
+// //     setTimeout(()=>{
+// //         callback(null,`data processed , ${data}`)
+// //     },1000)
+// // }
+// // function saveData(data,callback){
+// //     setTimeout(()=>{
+// //         callback(null,`data saved , ${data}`)
+// //     })
+// // }
+
+// // fetchData((err,data)=>{
+// //     if(err){
+// //         console.error(err)
+// //         return
+// //     }
+// //     processData(data,(err,processData)=>{
+// //         if(err){
+// //             console.error(err)
+// //             return
+// //         }
+// //         saveData(processData,(err,savedData)=>{
+// //             if(err){
+// //                 console.error(err)
+// //                 return
+// //             }
+// //             console.log(savedData)
+// //         })
+// //     })
+// // })
+
+// // -----------------------------------------------------------------------------------------------------
+// // convert callback hell into promise based code
+
+// function fetchData(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve('data fetched')
+//         },1000)
+//     })
+// }
+// function processData(data){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(`data processed , ${data}`)
+//         },1000)
+//     })
+// }
+// function saveData(data){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(`data saved , ${data}`)
+//         },1000)
+//     })
+// }
+// fetchData()
+// .then((data)=>processData(data))
+// .then((processedData)=>saveData(processedData))
+// .then((savedData)=>console.log(savedData))
+// .catch((err)=>console.error(err))
+
+
