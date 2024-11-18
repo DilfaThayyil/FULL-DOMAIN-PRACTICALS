91a88e9fb8f1de18d8d1ee67999e4754f8b8a5d2
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-19 00:01:51 +0530

    worker thread

---
+++ NODEJS/workerThread.js
@@ -0,0 +1,11 @@
+const {Worker,isMainThread,parentPort} = require('worker_threads')
+
+if(isMainThread){
+    const worker = new Worker(__filename)
+    worker.on('message',message=>console.log(message))
+    worker.postMessage('Do a heavy task')
+}else{
+    parentPort.on('message',()=>{
+        parentPor
+    })
+}
