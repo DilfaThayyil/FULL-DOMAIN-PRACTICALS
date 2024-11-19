628249398638105462f42c669d699b06d1b2d769
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-19 17:57:14 +0530

    practic

--- NODEJS/practice.js
+++ NODEJS/practice.js
@@ -131,3 +131,17 @@ if(cluster.isMaster){
         res.end('handled by worker')
     }).listen(8000)
 }
+
+//worker thread
+
+const {Worker,isMainThread,parentPort} = require('worker_threads')
+
+if(isMainThread){
+    const worker = new Worker(__filename)
+    worker.on('message',(message)=>console.log(message))
+    worker.postMessage('Do a heavy task..')
+}else{
+    parentPort.on('message',()=>{
+        parentPort.postMessage('task Done!')
+    })
+}
