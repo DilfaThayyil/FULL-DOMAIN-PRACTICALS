ed184d73e46d31361c71094b752a7da2cf4b5401
DilfaThayyil <dilfathayyil@gmail.com>
2024-11-30 09:01:47 +0530

    practice

--- DSA/practice.js
+++ DSA/practice.js
@@ -2799,64 +2799,64 @@
 
 
 
-class Graph{
-    constructor(){
-        this.adjacencyList = {}
-    }
-    addVertex(vertex){
-        this.adjacencyList[vertex] = new Set()
-    }
-    addEdge(vertex1,vertex2){
-        if(!this.adjacencyList[vertex1]){
-            this.addVertex(vertex1)
-        }
-        if(!this.adjacencyList[vertex2]){
-            this.addVertex(vertex2)
-        }
-        this.adjacencyList[vertex1].add(vertex2)
-        this.adjacencyList[vertex2].add(vertex1)
-    }
-    hasEdge(vertex1,vertex2){
-        return (
-            this.adjacencyList[vertex1].has(vertex2)&&
-            this.adjacencyList[vertex2].has(vertex1)
-        )
-    }
-    removeEdge(vertex1,vertex2){
-        this.adjacencyList[vertex1]?.delete(vertex2)
-        this.adjacencyList[vertex2]?.delete(vertex1)
-    }
-    removeVertex(vertex){
-        if(!this.adjacencyList[vertex]){
-            return
-        }
-        for(let neighbour of this.adjacencyList[vertex]){
-            this.adjacencyList[neighbour].delete(vertex)
-        }
-        delete this.adjacencyList[vertex]
-    }
-    display(){
-        for(let vertex in this.adjacencyList){
-            if(!this.adjacencyList[vertex]){
-                return
-            }
-            console.log(vertex,'=>',[...this.adjacencyList[vertex]])
-        }
-    }
-}
+// class Graph{
+//     constructor(){
+//         this.adjacencyList = {}
+//     }
+//     addVertex(vertex){
+//         this.adjacencyList[vertex] = new Set()
+//     }
+//     addEdge(vertex1,vertex2){
+//         if(!this.adjacencyList[vertex1]){
+//             this.addVertex(vertex1)
+//         }
+//         if(!this.adjacencyList[vertex2]){
+//             this.addVertex(vertex2)
+//         }
+//         this.adjacencyList[vertex1].add(vertex2)
+//         this.adjacencyList[vertex2].add(vertex1)
+//     }
+//     hasEdge(vertex1,vertex2){
+//         return (
+//             this.adjacencyList[vertex1].has(vertex2)&&
+//             this.adjacencyList[vertex2].has(vertex1)
+//         )
+//     }
+//     removeEdge(vertex1,vertex2){
+//         this.adjacencyList[vertex1]?.delete(vertex2)
+//         this.adjacencyList[vertex2]?.delete(vertex1)
+//     }
+//     removeVertex(vertex){
+//         if(!this.adjacencyList[vertex]){
+//             return
+//         }
+//         for(let neighbour of this.adjacencyList[vertex]){
+//             this.adjacencyList[neighbour].delete(vertex)
+//         }
+//         delete this.adjacencyList[vertex]
+//     }
+//     display(){
+//         for(let vertex in this.adjacencyList){
+//             if(!this.adjacencyList[vertex]){
+//                 return
+//             }
+//             console.log(vertex,'=>',[...this.adjacencyList[vertex]])
+//         }
+//     }
+// }
 
 
-const graph = new Graph()
-graph.addVertex('A')
-graph.addVertex('B')
-graph.addVertex('C')
-graph.addVertex('D')
-graph.addEdge('A','D')
-graph.addEdge('B','C')
-graph.addEdge('A','C')
-graph.addEdge('B','D')
-graph.display()
-console.log(graph.hasEdge('A','C'))
-graph.removeVertex('A')
-graph.removeEdge('C','B')
-graph.display()
+// const graph = new Graph()
+// graph.addVertex('A')
+// graph.addVertex('B')
+// graph.addVertex('C')
+// graph.addVertex('D')
+// graph.addEdge('A','D')
+// graph.addEdge('B','C')
+// graph.addEdge('A','C')
+// graph.addEdge('B','D')
+// graph.display()
+// console.log(graph.hasEdge('A','C'))
+// graph.removeVertex('A')
+// graph.removeEdge('C','B')
+// graph.display()
