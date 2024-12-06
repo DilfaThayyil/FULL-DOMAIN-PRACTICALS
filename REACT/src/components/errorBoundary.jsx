b52bcbcc0afd913997685c1c340dc0b5010e584d
DilfaThayyil <dilfathayyil@gmail.com>
2024-12-06 20:39:01 +0530

    fsdf

--- REACT/src/components/errorBoundary.jsx
+++ REACT/src/components/errorBoundary.jsx
@@ -1,25 +1,29 @@
-import React, { Component } from "react";
+const { Component } = require("react");
 
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.error("Error caught by ErrorBoundary:", error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+class ErrorBoundary extends Component{
+    constructor(props){
+        super(props)
+        this.state = {hasError:false,errorMessage:''}
     }
-    return this.props.children;
-  }
-}
 
-export default ErrorBoundary;
+    static getDerivedStateFromError(error){
+        return {hasError:true,errorMessage:error.message}
+    }
+
+    componentDidCatch(error,errorInfo){
+        console.error('Error caught by ErrorBoundary:',error,errorInfo)
+    }
+
+    render(){
+        if(this.state.hasError){
+            return (
+                <div>
+                    <h1>Oops! something went wrong.</h1>
+                    <p>{this.state.errorMessage}</p>
+                </div>
+            )
+        }
+        return this.props.children
+    }
+}
+export default ErrorBoundary
