67e8486359ebb52503bc034709390e87f40eb54f
DilfaThayyil <dilfathayyil@gmail.com>
2024-12-08 18:09:33 +0530

    error coundary

--- REACT/src/components/errorBoundary.jsx
+++ REACT/src/components/errorBoundary.jsx
@@ -1,24 +1,21 @@
-const { Component } = require("react");
+import React , {Component} from 'react'
 
 class ErrorBoundary extends Component{
     constructor(props){
         super(props)
         this.state = {hasError:false,errorMessage:''}
     }
-
     static getDerivedStateFromError(error){
         return {hasError:true,errorMessage:error.message}
     }
-
     componentDidCatch(error,errorInfo){
-        console.error('Error caught by ErrorBoundary:',error,errorInfo)
+        console.error('error caught up by errorboundary',error,errorInfo)
     }
-
     render(){
         if(this.state.hasError){
             return (
                 <div>
-                    <h1>Oops! something went wrong.</h1>
+                    <h1>Oops! something went wrong</h1>
                     <p>{this.state.errorMessage}</p>
                 </div>
             )
